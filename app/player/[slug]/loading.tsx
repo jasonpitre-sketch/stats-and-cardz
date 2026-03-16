5653abc00332254f216287b59107628f682bc444
Jason Pitre <jasonpitre@Victorias-iMac-2.local>
2026-03-16 13:35:15 -0300

    player profiles working

---
+++ app/player/[slug]/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Loading player...</h1>
+      <p>Fetching scouting report...</p>
+    </div>
+  )
+}
